4ac3aef7b20a276d5e4cd6476e4d04e2bdb2cc6c
Darren Ford <dford_au-reg@yahoo.com>
2026-03-02 17:48:21 +1100

    Minor file rename

--- Sources/AppKitUI/NSOpenPanel+appkitui.swift
+++ Sources/AppKitUI/NSOpenPanel+appkitui.swift
@@ -17,6 +17,8 @@
 //  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+// AppKitUI implementation for the open panel.
+
 import AppKit
 import UniformTypeIdentifiers
 import os.log
